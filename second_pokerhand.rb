61cd357fc9dd41931c5b422165c8a880976035a7
GabbyMont <gemo1236@gmail.com>
2018-05-08 16:28:27 -0400

    pair tiebreaker function/test working

--- second_pokerhand.rb
+++ second_pokerhand.rb
@@ -27,67 +27,112 @@ def compare_straights(hand1,hand2)
 end
 
 ############################################
-
 # look_at_tie function looks at each position(then deletes it from hand being evaluated) in hand and counts each dulpicated card
 def look_at_tie(hand1,hand2)
-	# Sets counter equal to one
-	counter = 1
-	# Sets match_card equal to 0
+	array_of_faces = []
+	arr_of_faces2 = []
+	result = ""
+	# counter = 0
 	match_card1 = 0
-	# Hand loop to evaluate each postion in array
-	hand1.each do |num|
-		# Deletes counter position(position in hand after evaluated)
-		hand_mock = hand1.delete_at(counter)
-		# If matched_card is the included number...
-		if hand_mock.include?(num)
-			# ...then the two numbers equal
-			match_card1 = num
-		end
-		counter += 1
-	end
-	match_card1
-
-	# Sets counter equal to one
-	counter = 1
-	# Sets match_card equal to 0
 	match_card2 = 0
-	# Hand loop to evaluate each postion in array
-	hand2.each do |num|
-		# Deletes counter position(position in hand after evaluated)
-		hand_mock = hand2.delete_at(counter)
-		# If matched_card is the included number...
-		if hand_mock.include?(num)
-			# ...then the two numbers equal
-			match_card2 = num
-		end
-		# Adds counter(to move through each position)
-		counter += 1
+	hand1.each do |card|
+		hand1_separator = card.split(//)
+		faces_to_integers = hand1_separator[0].to_i
+		array_of_faces << faces_to_integers
 	end
-	match_card2
-end
+	sorted_hand1 = array_of_faces.sort
+	hand1_dupes = sorted_hand1.join().match('(.)\1')[0]
+	hand1_duplicate_strings = hand1_dupes.split(//)
+	hand1_single_digit = hand1_duplicate_strings[0].to_i
+	# face_value = face_changer(face_value)
 
-def kicker_card(hand,matched)
-	new_hand = hand.delete(matched)
-	last_hand = hand.last
-end
+	
+	p hand1_duplicate_strings
+	p sorted_hand1
 
-def evaluate_tie(hand1,hand2)
-	hand1_match = look_at_tie(hand1)
-	hand2_match = look_at_tie(hand2)
-	if hand1 == hand2
-		high1 = kicker_card(hand1)
-		high2 = kicker_card(hand2)
-		if high1 > high2
-			"Hand One wins"
-		else high2 > high1
-			"Hand Two wins"
-		end
-	elsif hand1_match > hand2_match
-		"Hand One is the winner"
+	hand2.each do |card|
+    	hand2_separator = card.split(//)
+    	faces_to_int2 = hand2_separator[0].to_i
+		arr_of_faces2 << faces_to_int2
+	end
+	sorted_hand2 = arr_of_faces2.sort
+	hand2_dupes = sorted_hand2.join().match('(.)\1')[0]
+	hand2_duplicate_strings = hand2_dupes.split(//)
+	hand2_single_digit = hand2_duplicate_strings[0].to_i
+
+	p hand2_single_digit
+	p hand2_duplicate_strings
+	p sorted_hand2
+	
+	
+	if hand1_single_digit > hand2_single_digit
+		result = "hand1 wins"
 	else
-		"Hand Two is the winner"
+		result = "hand2 wins"
 	end
 end
+
+
+	# result = ""
+	# # Sets counter equal to one
+	# counter1 = 0
+	# # Sets match_card equal to 0
+	# match_card1 = 0
+	# # Hand loop to evaluate each postion in array
+	# hand1.each do |num|
+	# 	# Deletes counter position(position in hand after evaluated)
+	# 	# hand_mock = hand1.find_all(num)
+	# 	# If matched_card is the included number...
+	# 	if hand1.find_all(num)
+	# 		match_card1 = num
+	# 		new_hand1 << match_card1
+	# 		# ...then the two numbers equal
+	# 		# match_card1 = num
+	# 	end
+	# 	counter1 += 1
+	# end
+	# match_card1
+	# # Sets counter equal to one
+	# counter2 = 0
+	# # Sets match_card equal to 0
+	# match_card2 = 0
+	# # Hand loop to evaluate each postion in array
+	# hand2.each do |num|
+	# 	# Deletes counter position(position in hand after evaluated)
+	# 	hand_mock2 = hand2.find_all(num)
+	# 	# If matched_card is the included number...
+	# 	if hand_mock2.find_all(num)
+	# 		match_card2 = num
+	# 		new_hand2 << match_card2
+	# 		# ...then the two numbers equal
+	# 		# match_card2 = num
+	# 	end
+	# 	# Adds counter(to move through each position)
+	# 	counter2 += 1
+	# end
+	# match_card2
+
+
+# def kicker_card(hand,matched)
+# 	new_hand = hand.delete(matched)
+# 	last_hand = hand.last
+# end
+
+# def evaluate_tie(hands)
+# 	hands["hand1"] = hand1
+# 	hands["hand2"] = hand2
+# 	hand1_match = look_at_tie(hands["hand1"])
+# 	hand2_match = look_at_tie(hands["hand2"])
+# 	if hands["hand1"] == hands["hand2"]
+# 		high1 = kicker_card(hands["hand1"])
+# 		high2 = kicker_card(hands["hand2"])
+# 	end
+# 	if high1['hand1'] > high2['hand2']
+# 		"Hand One wins"
+# 	else high2['hand2'] > high1['hand1']
+# 		"Hand Two wins"
+# 	end
+# end
 #################################
 
 def face_changer(face_value)
@@ -144,35 +189,70 @@ def hand_comparison(hands)
 	elsif starter_hands["hand1"] < starter_hands["hand2"]
 		"Player Two is the winner"
 	# Else if cloned hand1 is equal to cloned hand2 then look at straight_tiebreaker
+	# else starter_hands["hand1"] == starter_hands["hand2"]
+	# 	straight_tiebreaker(hands['hand1'],hands['hand2'])
+	# end
 	else starter_hands["hand1"] == starter_hands["hand2"]
-		straight_tiebreaker(hands['hand1'],hands['hand2'])
+		look_at_tie(hands['hand1'],hands['hand2'])
 	end
 end
 
 
 # tie breaker is splitting the hands and gathering sum then returning winning score(highest sum)
-def straight_tiebreaker(hand1,hand2)
-  suit_value = []
-  face_value = []
-  hand1.each do |card|
-    face_value << card[0]
-    suit_value << card[1]
-  end
-    face_value = face_changer(face_value)
-    face_value.sort!
-    newarray = [*face_value[0]..face_value[0]+4]
-    array_sum = newarray.sum
+# def straight_tiebreaker(hand1,hand2)
+#   	suit_value = []
+#   	face_value = []
+#   	array_sum1 = []
+#   	array_sum2 = []
+#   	result = ""
+#   hand1.each do |card|
+#     face_value << card[0]
+#     suit_value << card[1]
+#   end
+#     face_value = face_changer(face_value)
+#     face_value.sort!
+#     newarray1 = [*face_value[0]..face_value[0]+4]
+#     array_sum1 = newarray1.sum
 
-  hand2.each do |card|
-    face_value << card[0]
-    suit_value << card[1]
-  end
-    face_value = face_changer(face_value)
-    face_value.sort!
-    newarray = [*face_value[0]..face_value[0]+4]
-    array_sum = newarray.sum
-end
+#   hand2.each do |card|
+#     face_value << card[0]
+#     suit_value << card[1]
+#   end
+#     face_value = face_changer(face_value)
+#     face_value.sort!
+#     newarray2 = [*face_value[0]..face_value[0]+4]
+#     array_sum2 = newarray2.sum
+#     if array_sum1 > array_sum2
+#     	result = "hand1 wins"
+#     else 
+#     	result = "hand2 wins"
+#     end
+#    	result
+# end
 
+# def pair_tiebreaker(hand1,hand2)
+#   	suit_value = []
+#   	face_value = []
+#   	result = ""
+#   hand1.group_by{ |card| card 
+#    	face_value << card[0]
+#     suit_value << card[1]}.select { |face_value| face_value.size > 1 }.map(&:first)
+#     face_value = face_changer(face_value)
+#     face_value.sort!
+# 	p "THIS IS HAND ONE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{hand1}"
+
+#   hand2.group_by{ |card| card 
+#     face_value << card[0]
+#     suit_value << card[1]}.select { |face_value| face_value.size > 1}.map(&:first)
+#  #hand2.find_all{|card| hand2.rindex(card) != hand2.index(card)}.uniq
+#     face_value = face_changer(face_value)
+#     face_value.sort!
+# 	if hand1 > hand2
+# 		result = "hand1 wins"
+# 	else
+# 		result = "hand2 wins"
+# 	end
+# end
 
 def royal_flush(hand)
 # Creates array for specific suit // Ex.) h(hearts)
@@ -186,7 +266,7 @@ face_value = []
 		# Pushes index[0] of card into face_value array(The second index of each string => suit: clubs..spades )
 		suit_value << card[1]
 	end
-	# If statement checking length of the suit value after suits are seperated from royal flush => should all be "d" for diamonds(uniq removes all duplicates making the length 1)
+	# If statement checking length of the suit value after suits are separated from royal flush => should all be "d" for diamonds(uniq removes all duplicates making the length 1)
 	if suit_value.uniq.length == 1
 		# Then if face_value inlcudes the ["A", "K", "Q", "J", "T"] faces...
 		if face_value.include?("A") && face_value.include?("K") && face_value.include?("Q") && face_value.include?("J") && face_value.include?("T")
--- second_tests.rb
+++ second_tests.rb
@@ -2,307 +2,321 @@ require 'minitest/autorun'
 require_relative 'second_pokerhand.rb'
 
 class Poker_Hand_test < Minitest::Test
-	def test_true_is_true
-		assert_equal(true,true)
-	end
+	# def test_true_is_true
+	# 	assert_equal(true,true)
+	# end
 
-	def test_hash_class
-		assert_equal(Hash,hand_builder().class)
-	end
+	# def test_hash_class
+	# 	assert_equal(Hash,hand_builder().class)
+	# end
 
-	def test_for_one_hands # Adjust later on to take two hands
-		assert_equal(2, hand_builder().length)
-	end
+	# def test_for_one_hands # Adjust later on to take two hands
+	# 	assert_equal(2, hand_builder().length)
+	# end
 
-	def test_compare_straights
-		assert_equal(true,compare_straights(4,3))
-	end
+	# def test_compare_straights
+	# 	assert_equal(true,compare_straights(4,3))
+	# end
 
-	def test_compare_straights
-		assert_equal(false,compare_straights(2,3))
-	end
+	# def test_compare_straights
+	# 	assert_equal(false,compare_straights(2,3))
+	# end
 
-	def test_hand1_length
-		hand_length1 = hand_builder()["hand1"]
-		hand_length2 = hand_builder()["hand2"]
-		assert_equal(5,hand_length1.length)
-		assert_equal(5,hand_length2.length)
-	end
+	# def test_hand1_length
+	# 	hand_length1 = hand_builder()["hand1"]
+	# 	hand_length2 = hand_builder()["hand2"]
+	# 	assert_equal(5,hand_length1.length)
+	# 	assert_equal(5,hand_length2.length)
+	# end
 
-	def test_no_duplicates
-		hand1 = hand_builder()["hand1"]
-		hand2 = hand_builder()["hand2"]
-		assert_equal(hand1,hand1.uniq)
-		assert_equal(hand2,hand2.uniq)
-	end
+	# def test_no_duplicates
+	# 	hand1 = hand_builder()["hand1"]
+	# 	hand2 = hand_builder()["hand2"]
+	# 	assert_equal(hand1,hand1.uniq)
+	# 	assert_equal(hand2,hand2.uniq)
+	# end
 
-	def test_royal_flush_class
-		hand = ["Ad", "Kd", "Qd", "Jd","Td"]
-		assert_equal(TrueClass,royal_flush(hand).class)
-	end
+	# def test_royal_flush_class
+	# 	hand = ["Ad", "Kd", "Qd", "Jd","Td"]
+	# 	assert_equal(TrueClass,royal_flush(hand).class)
+	# end
 
-	def test_royal_true
-		hand = ["Ad", "Kd", "Qd", "Jd","Td"]
-		assert_equal(true,royal_flush(hand))
-	end
+	# def test_royal_true
+	# 	hand = ["Ad", "Kd", "Qd", "Jd","Td"]
+	# 	assert_equal(true,royal_flush(hand))
+	# end
 
-	def test_royal_nil
-		hand = ["As", "Kd", "Qd", "Jd","Td"]
-		assert_nil(royal_flush(hand))
-	end
+	# def test_royal_nil
+	# 	hand = ["As", "Kd", "Qd", "Jd","Td"]
+	# 	assert_nil(royal_flush(hand))
+	# end
 
-	def test_straight_flush_class
-		hand = ["Ts", "9s", "7s", "8s","6s"]
-		assert_equal(TrueClass,straight_flush(hand).class)
-	end
+	# def test_straight_flush_class
+	# 	hand = ["Ts", "9s", "7s", "8s","6s"]
+	# 	assert_equal(TrueClass,straight_flush(hand).class)
+	# end
 
-	def test_striaght_flush_true
-		hand = ["Ts", "9s", "7s", "8s","6s"]
-		assert_equal(true,straight_flush(hand))
-	end
+	# def test_striaght_flush_true
+	# 	hand = ["Ts", "9s", "7s", "8s","6s"]
+	# 	assert_equal(true,straight_flush(hand))
+	# end
 
-	def test_striaght_flush_nil
-		hand = ["Ts", "3d", "7s", "8s","6s"]
-		assert_nil(straight_flush(hand))
-	end
+	# def test_striaght_flush_nil
+	# 	hand = ["Ts", "3d", "7s", "8s","6s"]
+	# 	assert_nil(straight_flush(hand))
+	# end
 
-	def test_face_changer
-		face_value = []
-		assert_equal(Array,face_changer(face_value).class)
-	end
+	# def test_face_changer
+	# 	face_value = []
+	# 	assert_equal(Array,face_changer(face_value).class)
+	# end
 
-	def test_for_A_14_facechanger
-		hand = ["A", "9", "6", "8","7"]
-		changed_hand = [14, 9, 6, 8,7]
-		assert_equal(changed_hand,face_changer(hand))
-	end
+	# def test_for_A_14_facechanger
+	# 	hand = ["A", "9", "6", "8","7"]
+	# 	changed_hand = [14, 9, 6, 8,7]
+	# 	assert_equal(changed_hand,face_changer(hand))
+	# end
 
-	def test_for_all_face_letters
-		hand = ["A", "J", "Q", "K","T"]
-		changed_hand = [14, 11, 12, 13, 10]
-		assert_equal(changed_hand,face_changer(hand))
-	end
+	# def test_for_all_face_letters
+	# 	hand = ["A", "J", "Q", "K","T"]
+	# 	changed_hand = [14, 11, 12, 13, 10]
+	# 	assert_equal(changed_hand,face_changer(hand))
+	# end
 
-	def test_four_of_a_kind_class
-		hand = ["2s", "2d", "2c", "2h","7s"]
-		assert_equal(TrueClass,four_of_a_kind(hand).class)
-	end
+	# def test_four_of_a_kind_class
+	# 	hand = ["2s", "2d", "2c", "2h","7s"]
+	# 	assert_equal(TrueClass,four_of_a_kind(hand).class)
+	# end
 
-	def test_four_of_a_kind_true
-		hand = ["2s", "2d", "2c", "2h","7s"]
-		assert_equal(true,four_of_a_kind(hand))
-	end
+	# def test_four_of_a_kind_true
+	# 	hand = ["2s", "2d", "2c", "2h","7s"]
+	# 	assert_equal(true,four_of_a_kind(hand))
+	# end
 
-	def test_four_of_a_kind_nil
-		hand = ["2s", "2d", "2c", "4h","7s"]
-		assert_nil(four_of_a_kind(hand))
-	end
+	# def test_four_of_a_kind_nil
+	# 	hand = ["2s", "2d", "2c", "4h","7s"]
+	# 	assert_nil(four_of_a_kind(hand))
+	# end
 
-	def test_three_of_a_kind_class
-		hand = ["4s", "4d", "4c", "2h","7s"]
-		assert_equal(TrueClass,three_of_a_kind(hand).class)
-	end
+	# def test_three_of_a_kind_class
+	# 	hand = ["4s", "4d", "4c", "2h","7s"]
+	# 	assert_equal(TrueClass,three_of_a_kind(hand).class)
+	# end
 
-	def test_three_of_a_kind_true
-		hand = ["4s", "4d", "4c", "2h","7s"]
-		assert_equal(true,three_of_a_kind(hand))
-	end
+	# def test_three_of_a_kind_true
+	# 	hand = ["4s", "4d", "4c", "2h","7s"]
+	# 	assert_equal(true,three_of_a_kind(hand))
+	# end
 
-	def test_for_three_of_a_kind_nil
-		hand = ["3s", "4d", "4c", "2h","7s"]
-		assert_nil(three_of_a_kind(hand))
-	end
+	# def test_for_three_of_a_kind_nil
+	# 	hand = ["3s", "4d", "4c", "2h","7s"]
+	# 	assert_nil(three_of_a_kind(hand))
+	# end
 
-	def test_pair_hand_class
-		hand = ["4s", "4d", "5c", "2h","7s"]
-		assert_equal(TrueClass,pair(hand).class)
-	end
+	# def test_pair_hand_class
+	# 	hand = ["4s", "4d", "5c", "2h","7s"]
+	# 	assert_equal(TrueClass,pair(hand).class)
+	# end
 
-	def test_pair_hand_true
-		hand = ["4s", "4d", "5c", "2h","7s"]
-		assert_equal(true,pair(hand))
-	end
+	# def test_pair_hand_true
+	# 	hand = ["4s", "4d", "5c", "2h","7s"]
+	# 	assert_equal(true,pair(hand))
+	# end
 
-	def test_pair_hand_nil
-		hand = ["6s", "4d", "5c", "2h","7s"]
-		assert_nil(pair(hand))
-	end
+	# def test_pair_hand_nil
+	# 	hand = ["6s", "4d", "5c", "2h","7s"]
+	# 	assert_nil(pair(hand))
+	# end
 
-	def test_full_house_class
-		hand = ["Ts", "Td", "Tc", "9h","9s"]
-		assert_equal(TrueClass,full_house(hand).class)
-	end
+	# def test_full_house_class
+	# 	hand = ["Ts", "Td", "Tc", "9h","9s"]
+	# 	assert_equal(TrueClass,full_house(hand).class)
+	# end
 
-	def test_full_house_true
-		hand = ["Ts", "Td", "Tc", "9h","9s"]
-		assert_equal(true,full_house(hand))
-	end
+	# def test_full_house_true
+	# 	hand = ["Ts", "Td", "Tc", "9h","9s"]
+	# 	assert_equal(true,full_house(hand))
+	# end
 
-	def test_full_house_nil
-		hand = ["4s", "Td", "Tc", "9h","9s"]
-		assert_nil(full_house(hand))
-	end
+	# def test_full_house_nil
+	# 	hand = ["4s", "Td", "Tc", "9h","9s"]
+	# 	assert_nil(full_house(hand))
+	# end
 
-	def test_flush_class
-		hand = ["1s", "5s", "Ts","9s","7s"]
-		assert_equal(TrueClass,flush(hand).class)
-	end
+	# def test_flush_class
+	# 	hand = ["1s", "5s", "Ts","9s","7s"]
+	# 	assert_equal(TrueClass,flush(hand).class)
+	# end
 
-	def test_flush_true
-		hand = ["1s", "5s", "Ts", "9s","7s"]
-		assert_equal(true,flush(hand))
-	end
+	# def test_flush_true
+	# 	hand = ["1s", "5s", "Ts", "9s","7s"]
+	# 	assert_equal(true,flush(hand))
+	# end
 
-	def test_flush_nil
-		hand = ["4d", "5s", "Ts", "9s","7s"]
-		assert_nil(flush(hand))
-	end
+	# def test_flush_nil
+	# 	hand = ["4d", "5s", "Ts", "9s","7s"]
+	# 	assert_nil(flush(hand))
+	# end
 
-	def test_flush_face_true
-		hand = ["Qs", "Ks", "Ts","Js","As"]
-		assert_equal(true,flush(hand))
-	end
+	# def test_flush_face_true
+	# 	hand = ["Qs", "Ks", "Ts","Js","As"]
+	# 	assert_equal(true,flush(hand))
+	# end
 
-	def test_straight_class
-		hand = ["Td", "9s", "6c", "8s","7h"]
-		assert_equal(TrueClass,straight(hand).class)
-	end
+	# def test_straight_class
+	# 	hand = ["Td", "9s", "6c", "8s","7h"]
+	# 	assert_equal(TrueClass,straight(hand).class)
+	# end
 
-	def test_striaght_true
-		hand = ["Td", "9s", "6c", "8s","7h"]
-		assert_equal(true,straight(hand))
-	end
+	# def test_striaght_true
+	# 	hand = ["Td", "9s", "6c", "8s","7h"]
+	# 	assert_equal(true,straight(hand))
+	# end
 
-	def test_striaght_nil
-		hand = ["2d", "9s", "6c", "8s","7h"]
-		assert_nil(straight(hand))
-	end
+	# def test_striaght_nil
+	# 	hand = ["2d", "9s", "6c", "8s","7h"]
+	# 	assert_nil(straight(hand))
+	# end
 
-	def test_two_pair_class
-		hand = ["4s", "4d", "2c", "2h","7s"]
-		assert_equal(TrueClass,two_pair(hand).class)
-	end
+	# def test_two_pair_class
+	# 	hand = ["4s", "4d", "2c", "2h","7s"]
+	# 	assert_equal(TrueClass,two_pair(hand).class)
+	# end
 
-	def test_two_pair_true
-		hand = ["4s", "4d", "2c", "2h","7s"]
-		assert_equal(true,two_pair(hand))
-	end
+	# def test_two_pair_true
+	# 	hand = ["4s", "4d", "2c", "2h","7s"]
+	# 	assert_equal(true,two_pair(hand))
+	# end
 
-	def test_two_pair_nil
-		hand = ["5s", "4d", "2c", "2h","7s"]
-		assert_nil(two_pair(hand))
-	end
+	# def test_two_pair_nil
+	# 	hand = ["5s", "4d", "2c", "2h","7s"]
+	# 	assert_nil(two_pair(hand))
+	# end
 
-	def test_hand_comp_class
-		hands = {"hand1" => ["7h", "3d", "4d", "6s", "5s"],"hand2" => ["9h", "3d", "4d", "6s", "5h"]}
-		assert_equal(String,hand_comparison(hands).class)
-	end
+	# def test_hand_comp_class
+	# 	hands = {"hand1" => ["7h", "3d", "4d", "6s", "5s"],"hand2" => ["9h", "3d", "4d", "6s", "5h"]}
+	# 	assert_equal(String,hand_comparison(hands).class)
+	# end
 
-	def test_hand_comparison
-		hand1 = ["7h", "3d", "4d", "6s", "5s"]
-		hand2 = ["9h", "3d", "4d", "6s", "5h"]
+	# def test_hand_comparison
+	# 	hand1 = ["7h", "3d", "4d", "6s", "5s"]
+	# 	hand2 = ["9h", "3d", "4d", "6s", "5h"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner", hand_comparison(hands))
+	# end
+
+	# def test_high_card_class
+	# 	hand = ["7s", "3d", "4c", "5h","6s"]
+	# 	assert_equal(TrueClass,high_card(hand).class)
+	# end
+
+	# def test_high_card_true
+	# 	hand = ["7s", "4d", "3c", "5h","6s"]
+	# 	assert_equal(true,high_card(hand))
+	# end
+
+	# def test_two_cards_for_royal_flush_winner
+	# 	hand1 = ["Ad", "Kd", "Qd", "Jd","Td"]
+	# 	hand2 = ["9h", "3d", "4d", "6s", "5h"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner",hand_comparison(hands))
+	# end
+
+	# def test_two_straight_flushes
+	# 	hand1 = ["Ad", "Kd", "Qd", "Jd","Td"]
+	# 	hand2 = ["Ts", "9s", "7s", "8s","6s"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner",hand_comparison(hands))
+	# end
+
+	# def test_two_flushes
+	# 	hand1 = ["1s", "5s", "Ts", "9s","7s"]
+	# 	hand2 = ["Ts", "9s", "7s", "8s","6s"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player Two is the winner",hand_comparison(hands))
+	# end
+
+	# def test_straight_win
+	# 	hand1 = ["Ts", "9s", "7s", "8s","6s"]
+	# 	hand2 = ["As", "9s", "7s", "8s","6s"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner",hand_comparison(hands))
+	# end
+
+	# def test_str_win_not_four_win
+	# 	hand1 = ["Ts", "9s", "7s", "8s","6s"]
+	# 	hand2 = ["2s", "2d", "2c", "4h","7s"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner",hand_comparison(hands))
+	# end
+
+	# def test_four_of_a_kind_tie
+	# 	hand1 = ["2s", "2d", "2c", "2h","7s"]
+	# 	hand2 = ["2s", "2d", "2c", "2h","7s"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal(20,hand_comparison(hands))
+	# end
+
+	# def test_fullhouse_win
+	# 	hand1 = ["2s", "9s", "9c", "8s","6s"]
+	# 	hand2 = ["Js", "Jd", "Jc", "Th","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player Two is the winner",hand_comparison(hands))
+	# end
+
+	# def test_fullhouse_tie
+	# 	hand1 = ["9s", "9h", "9c", "8d","8s"]
+	# 	hand2 = ["Js", "Jd", "Jc", "Th","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal(50,hand_comparison(hands))
+	# end
+
+	# def test_flush_winner
+	# 	hand1 = ["2d", "9d", "3d", "6d","8d"]
+	# 	hand2 = ["Js", "Jd", "Jc", "8h","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player One is the winner",hand_comparison(hands))
+	# end
+
+	# def test_two_pair_win_against
+	# 	hand1 = ["9s", "9h", "Tc", "4d","8s"]
+	# 	hand2 = ["Js", "Jd", "8c", "Th","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player Two is the winner",hand_comparison(hands))
+	# end
+
+	# def test_two_pair_win_against
+	# 	hand1 = ["9s", "9h", "Tc", "4d","8s"]
+	# 	hand2 = ["Js", "Jd", "8c", "Th","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("Player Two is the winner",hand_comparison(hands))
+	# end
+
+	# def test_hand_two_high_card
+	# 	hand1 = ["2s", "9h", "4d", "3s","6c"]
+	# 	hand2 = ["Js", "2d", "6c", "8h","Ts"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal(20,hand_comparison(hands))
+	# end
+
+	# def test_tiebreaker_in_handcomp
+	# 	hand1 = ["9s", "5s", "7s", "8s","6s"]
+	# 	hand2 = ["2d", "3d", "4d", "5d","6d"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal(20,hand_comparison(hands))
+	# end
+
+	# def test_evaluate_hands
+	# 	hand2 = ["Td", "9s", "6c", "8s","7h"]
+	# 	hand1 = ["2d", "3d", "4s", "5d","6d"]
+	# 	hands = {"hand1" => hand1,"hand2" => hand2}
+	# 	assert_equal("hand2 wins",hand_comparison(hands))
+	# end
+
+	def test_pair_tiebreaker
+		hand1 = ["4s", "7d", "5c", "2h","7s"]
+		hand2 = ["4s", "4d", "5c", "2h","7s"]
 		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner", hand_comparison(hands))
-	end
-
-	def test_high_card_class
-		hand = ["7s", "3d", "4c", "5h","6s"]
-		assert_equal(TrueClass,high_card(hand).class)
-	end
-
-	def test_high_card_true
-		hand = ["7s", "4d", "3c", "5h","6s"]
-		assert_equal(true,high_card(hand))
-	end
-
-	def test_two_cards_for_royal_flush_winner
-		hand1 = ["Ad", "Kd", "Qd", "Jd","Td"]
-		hand2 = ["9h", "3d", "4d", "6s", "5h"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner",hand_comparison(hands))
-	end
-
-	def test_two_straight_flushes
-		hand1 = ["Ad", "Kd", "Qd", "Jd","Td"]
-		hand2 = ["Ts", "9s", "7s", "8s","6s"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner",hand_comparison(hands))
-	end
-
-	def test_two_flushes
-		hand1 = ["1s", "5s", "Ts", "9s","7s"]
-		hand2 = ["Ts", "9s", "7s", "8s","6s"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player Two is the winner",hand_comparison(hands))
-	end
-
-	def test_straight_win
-		hand1 = ["Ts", "9s", "7s", "8s","6s"]
-		hand2 = ["As", "9s", "7s", "8s","6s"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner",hand_comparison(hands))
-	end
-
-	def test_str_win_not_four_win
-		hand1 = ["Ts", "9s", "7s", "8s","6s"]
-		hand2 = ["2s", "2d", "2c", "4h","7s"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner",hand_comparison(hands))
-	end
-
-	def test_four_of_a_kind_tie
-		hand1 = ["2s", "2d", "2c", "2h","7s"]
-		hand2 = ["2s", "2d", "2c", "2h","7s"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal(20,hand_comparison(hands))
-	end
-
-	def test_fullhouse_win
-		hand1 = ["2s", "9s", "9c", "8s","6s"]
-		hand2 = ["Js", "Jd", "Jc", "Th","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player Two is the winner",hand_comparison(hands))
-	end
-
-	def test_fullhouse_tie
-		hand1 = ["9s", "9h", "9c", "8d","8s"]
-		hand2 = ["Js", "Jd", "Jc", "Th","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal(50,hand_comparison(hands))
-	end
-
-	def test_flush_winner
-		hand1 = ["2d", "9d", "3d", "6d","8d"]
-		hand2 = ["Js", "Jd", "Jc", "8h","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player One is the winner",hand_comparison(hands))
-	end
-
-	def test_two_pair_win_against
-		hand1 = ["9s", "9h", "Tc", "4d","8s"]
-		hand2 = ["Js", "Jd", "8c", "Th","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player Two is the winner",hand_comparison(hands))
-	end
-
-	def test_two_pair_win_against
-		hand1 = ["9s", "9h", "Tc", "4d","8s"]
-		hand2 = ["Js", "Jd", "8c", "Th","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal("Player Two is the winner",hand_comparison(hands))
-	end
-
-	def test_hand_two_high_card
-		hand1 = ["2s", "9h", "4d", "3s","6c"]
-		hand2 = ["Js", "2d", "6c", "8h","Ts"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal(20,hand_comparison(hands))
-	end
-
-	def test_tiebreaker_in_handcomp
-		hand1 = ["9s", "5s", "7s", "8s","6s"]
-		hand2 = ["2d", "3d", "4d", "5d","6d"]
-		hands = {"hand1" => hand1,"hand2" => hand2}
-		assert_equal(20,hand_comparison(hands))
+		assert_equal("hand1 wins",hand_comparison(hands))
 	end
 end
